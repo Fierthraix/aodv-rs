e6f735ff867905263a76278dec4e2ea028d94953
Lucas Rooyakkers <lucas.rooyakkers@protonmail.com>
2017-11-02 14:22:02 -0400

    Added framework for aodv and internal servers

--- src/server.rs
+++ src/server.rs
@@ -1,25 +1,55 @@
 use std::io;
-use std::net::SocketAddr;
+use std::net::{Ipv4Addr, SocketAddr};
 
-use futures::{Future, Poll};
+use futures::{Future, Poll, Sink, Stream};
 use tokio_core::net::UdpSocket;
 use tokio_core::reactor::Core;
 
+use aodv::*;
 use parse::Config;
 use routing::RoutingTable;
 
+const AODV_PORT: u16 = 654;
+const INSTANCE_PORT: u16 = 15292;
+
+/// Outward AOV server
 pub fn aodv(config: &Config, routing_table: RoutingTable) {
 
-    //TODO: Check port is being used
+    // Get address
+    let addr = SocketAddr::new("0.0.0.0".parse().unwrap(), AODV_PORT);
 
-    //TODO: Handle messages from spun up instances
+    // Get new core/handle
+    let mut core = Core::new().unwrap();
+    let handle = core.handle();
 
+    //  Bind to Address
+    let socket = UdpSocket::bind(&addr, &handle).unwrap();
+    println!("Started listening on {}", AODV_PORT);
+
+    // Get sink/stream for AODV codec
+    let (sink, stream) = socket.framed(AodvCodec).split();
+
+    // Handle incoming AODV messages
+    let stream = stream
+        .map(|(addr, msg)| { msg.handle_message(addr); })
+        .and_then(|_| Ok(()));
+
+    // Start server
+    //TODO: Get this working!
+    //let server = core.run(stream);
 }
 
+/// Internal instance server
 pub fn server(config: &Config) {
-    //TODO: bind udp port and listen
 
-    //TODO: handle received aodv control messages
+    // Get address
+    let addr = SocketAddr::new("0.0.0.0".parse().unwrap(), INSTANCE_PORT);
+
+    // Get new core/handle
+    let mut core = Core::new().unwrap();
+    let handle = core.handle();
+
+    //TODO: Handle messages from spun up instances
 }
 
 struct Server;

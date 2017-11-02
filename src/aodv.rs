b45ba5bd7322cb834242524a17c66b2ad72cb391
Lucas Rooyakkers <lucas.rooyakkers@protonmail.com>
2017-11-02 12:41:45 -0400

    Added UdpCodec for aodv messages

--- src/aodv.rs
+++ src/aodv.rs
@@ -1,24 +1,22 @@
+extern crate tokio_core;
+
 use std::io;
-use std::error;
-use std::fmt;
-use std::net::Ipv4Addr;
+use std::net::{Ipv4Addr, SocketAddr};
 
 use rreq::*;
 use rrep::*;
 use rerr::*;
 
-#[derive(Debug)]
+use tokio_core::net::{UdpSocket, UdpCodec};
+
 pub struct ParseError;
 
-impl error::Error for ParseError {
-    fn description(&self) -> &str {
-        "Unable to parse bit message as AODV message"
-    }
-}
-
-impl fmt::Display for ParseError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Unable to parse bit message as AODV message")
+impl ParseError {
+    pub fn new() -> io::Error {
+        io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "Unable to parse bit message as AODV message",
+        )
     }
 }
 
@@ -31,35 +29,38 @@ pub enum AodvMessage {
 }
 
 impl AodvMessage {
-    pub fn parse(b: &[u8]) -> Result<(), ParseError> {
+    pub fn parse(b: &[u8]) -> Result<Self, io::Error> {
         if b.len() == 0 {
-            return Err(ParseError {});
+            return Err(ParseError::new());
         }
         // Type, Length, Multiple of 4
         match (b[0], b.len(), b.len() % 4) {
-            (1, 24, 0) => {
-                match RREQ::new(b) {
-                    Ok(r) => Ok(r.handle_message()),
-                    Err(e) => Err(e),
-                }
-            }
-            (2, 20, 0) => {
-                match RREP::new(b) {
-                    Ok(r) => Ok(r.handle_message()),
-                    Err(e) => Err(e),
-                }
-            }
-            (3, _, 0) => {
-                match RERR::new(b) {
-                    Ok(r) => Ok(r.handle_message()),
-                    Err(e) => Err(e),
-                }
-            }
-            (4, 2, 2) => {
-                println!("rerr message!");
-                Ok(())
-            }
-            (_, _, _) => Err(ParseError),
+            (1, 24, 0) => Ok(AodvMessage::Rreq(RREQ::new(b)?)),
+            (2, 20, 0) => Ok(AodvMessage::Rrep(RREP::new(b)?)),
+            (3, _, 0) => Ok(AodvMessage::Rerr(RERR::new(b)?)),
+            (4, 2, 2) => Ok(AodvMessage::Ack),
+            (_, _, _) => Err(ParseError::new()),
         }
+    }
+    pub fn bit_message() -> Vec<u8> {
+        //TODO Add the real bit message
+        return vec![1, 2, 3];
+    }
+}
+
+pub struct AodvCodec;
+
+impl UdpCodec for AodvCodec {
+    type In = (SocketAddr, AodvMessage);
+    type Out = (SocketAddr, AodvMessage);
+
+    fn decode(&mut self, addr: &SocketAddr, buf: &[u8]) -> Result<Self::In, io::Error> {
+        Ok((*addr, AodvMessage::parse(buf)?))
+    }
+
+    fn encode(&mut self, (addr, msg): Self::Out, into: &mut Vec<u8>) -> SocketAddr {
+        //TODO: fix this
+        into.extend(vec![1, 2, 3]);
+        addr
     }
 }
